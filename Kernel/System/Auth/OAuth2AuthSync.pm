5b7012878e1ad56718560acf05ca2ec52323d482
hack3d <hack3d.dev@googlemail.com>
2026-08-18 22:15:13 +0200

    Create releases on github

--- Kernel/System/Auth/OAuth2AuthSync.pm
+++ Kernel/System/Auth/OAuth2AuthSync.pm
@@ -1,13 +1,13 @@
-package Kernel::System::Auth::OAuth2AuthSync;
+package Kernel::System::AuthSync::OAuth2AuthSync;
 
 use strict;
 use warnings;
 
 our @ObjectDependencies = (
     'Kernel::Config',
-    'Kernel::System::Log',
     'Kernel::System::User',
     'Kernel::System::Group',
+    'Kernel::System::Log',
 );
 
 sub new {
@@ -19,27 +19,24 @@ sub new {
     return $Self;
 }
 
-sub Auth {
+sub Sync {
     my ( $Self, %Param ) = @_;
+
+    my $UserLogin = $Param{User};
+
+    return if !$UserLogin;
 
     my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
     my $UserObject   = $Kernel::OM->Get('Kernel::System::User');
     my $GroupObject  = $Kernel::OM->Get('Kernel::System::Group');
     my $LogObject    = $Kernel::OM->Get('Kernel::System::Log');
 
-    my $UserLogin =
-           $ENV{REMOTE_USER}
-        || $ENV{HTTP_X_REMOTE_EMAIL}
-        || $ENV{HTTP_X_AUTH_REQUEST_EMAIL}
-        || '';
-
-    return if !$UserLogin;
-
-    my %Protected = map { $_ => 1 }
+    my @ProtectedUsers =
         @{ $ConfigObject->Get('OAuth2AuthSync::ProtectedUsers') || [] };
 
-    return ( $UserLogin, 1 )
-        if $Protected{$UserLogin};
+    my %Protected = map { $_ => 1 } @ProtectedUsers;
+
+    return 1 if $Protected{$UserLogin};
 
     my $Email =
            $ENV{HTTP_X_REMOTE_EMAIL}
@@ -56,6 +53,16 @@ sub Auth {
         || $ENV{HTTP_X_AUTH_REQUEST_FAMILY_NAME}
         || '';
 
+    my $DisplayName =
+           $ENV{HTTP_X_REMOTE_NAME}
+        || $ENV{HTTP_X_AUTH_REQUEST_NAME}
+        || '';
+
+    if ( !$FirstName && $DisplayName =~ /^(.+?)\s+(.+)$/ ) {
+        $FirstName = $1;
+        $LastName  = $2;
+    }
+
     my $UserID = $UserObject->UserLookup(
         UserLogin => $UserLogin,
     );
@@ -63,9 +70,9 @@ sub Auth {
     if (!$UserID) {
 
         $UserID = $UserObject->UserAdd(
+            UserLogin     => $UserLogin,
             UserFirstname => $FirstName || 'OAuth2',
             UserLastname  => $LastName  || 'User',
-            UserLogin     => $UserLogin,
             UserEmail     => $Email,
             ValidID       => 1,
             ChangeUserID  => 1,
@@ -75,60 +82,61 @@ sub Auth {
             Priority => 'notice',
             Message  => "OAuth2AuthSync created user <$UserLogin>",
         );
+
+        return if !$UserID;
     }
 
-    if ($UserID) {
-
-        $UserObject->UserUpdate(
-            UserID        => $UserID,
-            UserFirstname => $FirstName,
-            UserLastname  => $LastName,
-            UserEmail     => $Email,
-            ChangeUserID  => 1,
-        );
-    }
+    $UserObject->UserUpdate(
+        UserID        => $UserID,
+        UserFirstname => $FirstName,
+        UserLastname  => $LastName,
+        UserEmail     => $Email,
+        ValidID       => 1,
+        ChangeUserID  => 1,
+    );
 
     my $GroupsHeader =
            $ENV{HTTP_X_REMOTE_GROUPS}
         || $ENV{HTTP_X_AUTH_REQUEST_GROUPS}
+        || $ENV{HTTP_X_FORWARDED_GROUPS}
         || '';
 
     my %GroupMapping =
         %{ $ConfigObject->Get('OAuth2AuthSync::GroupMapping') || {} };
 
-    if ($UserID && $GroupsHeader) {
+    return 1 if !$GroupsHeader;
 
-        my @EntraGroups = split /\s*,\s*/, $GroupsHeader;
+    my @EntraGroups = split /\s*,\s*/, $GroupsHeader;
 
-        GROUP:
-        for my $EntraGroup (@EntraGroups) {
+    my %WantedGroups;
 
-            my $ZnunyGroup = $GroupMapping{$EntraGroup};
+    GROUP:
+    for my $EntraGroup (@EntraGroups) {
 
-            next GROUP if !$ZnunyGroup;
+        my $ZnunyGroup = $GroupMapping{$EntraGroup};
 
-            my $GroupID = $GroupObject->GroupLookup(
-                Group => $ZnunyGroup,
-            );
+        next GROUP if !$ZnunyGroup;
 
-            next GROUP if !$GroupID;
+        my $GroupID = $GroupObject->GroupLookup(
+            Group => $ZnunyGroup,
+        );
 
-            for my $Permission (qw(ro rw)) {
+        next GROUP if !$GroupID;
 
-                $GroupObject->PermissionGroupUserAdd(
-                    UID           => $UserID,
-                    GID           => $GroupID,
-                    PermissionKey => $Permission,
-                    UserID        => 1,
-                );
-            }
-        }
+        $WantedGroups{$GroupID} = 1;
     }
 
-    return (
-        $UserLogin,
-        1,
-    );
+    for my $GroupID ( keys %WantedGroups ) {
+
+        $GroupObject->PermissionGroupUserAdd(
+            UID           => $UserID,
+            GID           => $GroupID,
+            PermissionKey => 'rw',
+            UserID        => 1,
+        );
+    }
+
+    return 1;
 }
 
 1;
